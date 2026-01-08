92de00bba3ab85128cdcd03efc159941fde12867
pvtrex <zhenrime@gmail.com>
2026-01-08 08:24:43 +0530

    hero section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,29 @@
-import React from 'react'
-import { navLinks } from '../constants'
+import {navLinks} from "../constants";
 
-function NavBar() {
+const NavBar = () => {
     return (
         <header>
             <nav>
-
-                <img src="/logo.svg" alt="Apple Logo" />
+                <img  src="/logo.svg" alt="Apple logo" />
 
                 <ul>
-                    {navLinks.map((link) => (
-                        <li key={link.label}>
-                            <a href={link.label}>{link.label}</a>
+                    {navLinks.map(({ label }) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
                         </li>
-                    )
-                    )}
+                    ))}
                 </ul>
 
-
-
-                <div className="flex-center gap-3"><button><img src="/search.svg" alt="Search" /></button>
-                <button><img src="/cart.svg" alt="Cart" /></button></div>
-
-
-
+                <div className="flex-center gap-3">
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+                </div>
             </nav>
-
         </header>
     )
 }
-
 export default NavBar
-
-
-
---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div>
+       hi
+    </div>
+  )
+}
+
+export default Product
