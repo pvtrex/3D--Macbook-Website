0b0bdf4fa5d28eaaf34c01593acc9b980583e86f
pvtrex <zhenrime@gmail.com>
2026-01-09 15:39:20 +0530

    model switcher

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -22,7 +22,7 @@ const isMobile =useMediaQuery({query: '(max-width:1024px)'})
             <h2>Take a closer look</h2>
 
             <div className="controls">
-          ``      <p className="info">MacbookPro {scale} in {color}</p>
+          ``      <p className="info">MacbookPro | Available in 14" & 16" in Space Gray & Dark colors</p>
 
                <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
