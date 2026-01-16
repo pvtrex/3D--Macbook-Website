950328096891da5ad6d81e60dac6a8d029aee056
pvtrex <zhenrime@gmail.com>
2026-01-16 15:34:58 +0530

    Highlight and Performance Section completetd

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import ProductViewer from './components/ProductViewer'
-import Footer from './components/Footer'
-import gsap from 'gsap'
-import { ScrollTrigger} from 'gsap/all'
-import Showcase from './components/Showcase'
-import Features from './components/Features'
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
 
 gsap.registerPlugin(ScrollTrigger)
 
-function App() {
-  return (
-    <main>
-      <NavBar/>
-      <Hero/>
-      <ProductViewer/>
-      <Showcase/>
-      <Features/>
-      <Footer/>
-    </main>
-  )
+const App = () => {
+    return (
+        <main>
+            <NavBar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
+        </main>
+    )
 }
 
 export default App
-
-
