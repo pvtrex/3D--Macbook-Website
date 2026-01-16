10a1d7784cdfe3923aee671cd3c12e4559349b49
pvtrex <zhenrime@gmail.com>
2026-01-16 12:59:42 +0530

    Features Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
+import Footer from './components/Footer'
 import gsap from 'gsap'
 import { ScrollTrigger} from 'gsap/all'
 import Showcase from './components/Showcase'
+import Features from './components/Features'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -15,6 +17,8 @@ function App() {
       <Hero/>
       <ProductViewer/>
       <Showcase/>
+      <Features/>
+      <Footer/>
     </main>
   )
 }
